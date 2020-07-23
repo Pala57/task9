a40a349383660a9e04f85875d761f7553a2069e5
Олександр Паламарчук <a.palamar4uck@gmail.com>
2020-07-23 11:37:10 +0300

    Add project files.

---
+++ task9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+                      System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            char N;
+
+
+
+            bool f;
+
+
+            int a;
+
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Ввести N= ");
+                    f = char.TryParse(Console.ReadLine(), out N);
+                    if (!f)
+                    {
+                        Console.WriteLine("Помилка введіть ще раз");
+                    }
+                } while (!f);
+                a = (int)N - 32;
+                Console.WriteLine((char)a);
+            } while (N != '.');
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
